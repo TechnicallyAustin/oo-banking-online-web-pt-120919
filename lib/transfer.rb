87b2a995ff5978a9d6e9dd7f15d1be4af27cf3f3
Austin Johnson <technicallyaustin@gmail.com>
2020-01-30 20:45:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,7 @@ class Transfer
   if @sender.balance > @amount && @status == "pending"
     @sender.balance -= @amount
     @receiver.balance += @amount 
-    @status == "complete"
+    @status = "complete"
   else
   @status = "rejected"
   "Transaction rejected. Please check your account balance."
