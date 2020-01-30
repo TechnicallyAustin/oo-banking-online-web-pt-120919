bb3f56da795d055ecf04943d13484b54b8b8d014
Austin Johnson <technicallyaustin@gmail.com>
2020-01-30 20:50:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,7 @@ class Transfer
     @sender.balance -= @amount
     @receiver.balance += @amount 
     @status = "complete"
-  elsif @sender.balance < @amount
+  elsif @sender.balance < @amount && @status == "closed"
   self.status = "rejected"
   "Transaction rejected. Please check your account balance."
   binding.pry
